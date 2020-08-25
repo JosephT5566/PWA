90036c7526b91e5a6447d83e7fc9e69cd7ce10e2
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-08-25 15:41:11 +0800

    Updates

--- precache-manifest.d516428c6edab83e4e4bbabe4b9a7a33.js
+++ precache-manifest.d516428c6edab83e4e4bbabe4b9a7a33.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4fd923be00493eefee91651bf803317a",
+    "revision": "5034a8e1cfa55726195a6baf80c112b1",
     "url": "/pwa/index.html"
   },
   {
-    "revision": "1cfa2228d1bdfc1f319d",
+    "revision": "2e4fa01999d53be9b2d3",
     "url": "/pwa/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "b4c3a2cad1a98f8cb215",
-    "url": "/pwa/static/js/2.4fac61c6.chunk.js"
+    "revision": "c06e30b27e106e1207f7",
+    "url": "/pwa/static/js/2.d1346210.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/pwa/static/js/2.4fac61c6.chunk.js.LICENSE.txt"
+    "url": "/pwa/static/js/2.d1346210.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1cfa2228d1bdfc1f319d",
-    "url": "/pwa/static/js/main.c6ad357a.chunk.js"
+    "revision": "2e4fa01999d53be9b2d3",
+    "url": "/pwa/static/js/main.14ca0c58.chunk.js"
   },
   {
     "revision": "21f973f21343ad358a3d",
