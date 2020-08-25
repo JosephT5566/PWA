7b83afe9c92a462fc07154a18ef9977cefedc047
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-08-25 18:55:47 +0800

    Updates

--- precache-manifest.95431715b58b80925f7adc7d5a68cad1.js
+++ precache-manifest.95431715b58b80925f7adc7d5a68cad1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5034a8e1cfa55726195a6baf80c112b1",
+    "revision": "f82a68f0303534248a2d46cb2bd9cfe7",
     "url": "/pwa/index.html"
   },
   {
-    "revision": "2e4fa01999d53be9b2d3",
+    "revision": "742c3b25ad121805a7ca",
     "url": "/pwa/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pwa/static/js/2.d1346210.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e4fa01999d53be9b2d3",
-    "url": "/pwa/static/js/main.14ca0c58.chunk.js"
+    "revision": "742c3b25ad121805a7ca",
+    "url": "/pwa/static/js/main.88fc7631.chunk.js"
   },
   {
     "revision": "21f973f21343ad358a3d",
