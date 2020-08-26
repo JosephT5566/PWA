e70c889bb6f9056864f16976b5143607e08ce40b
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-08-26 18:40:31 +0800

    Display Drewer when screen large 1.  add display flex to app 2. add Hidden to hide and show different component on different screen width

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Footer from './components/Footer';
 import ProfilePage from './ProfilePage';
 import MainPage from './MainPage';
 
+const useStyles = makeStyles({
+    app: {
+        display: 'flex',
+    },
+    content: {
+        flexGrow: 1,
+    },
+});
+
 function App() {
+    const classes = useStyles();
     return (
-        <div className="App">
+        <div className={classes.app}>
             <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <Switch>
-                    <Route path="/" exact component={MainPage} />
-                    <Route path="/profile-page" component={ProfilePage} />
-                    <Route component={() => <div>404 Not found </div>} />
-                </Switch>
                 <Footer />
+                <div className={classes.content}>
+                    <Switch>
+                        <Route path="/" exact component={MainPage} />
+                        <Route path="/profile-page" component={ProfilePage} />
+                        <Route component={() => <div>404 Not found </div>} />
+                    </Switch>
+                </div>
             </BrowserRouter>
         </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Hidden from '@material-ui/core/Hidden';
 import FolderIcon from '@material-ui/icons/Folder';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import HomeIcon from '@material-ui/icons/Home';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles({
-    root: {
+    drewer: {
+        width: 140,
+    },
+    bottomNavigation: {
         position: 'absolute',
         bottom: 0,
         width: '100%',
@@ -24,23 +33,43 @@ export default function Footer() {
         setValue(newValue);
     };
     return (
-        <div className={classes.root}>
-            <BottomNavigation value={value} onChange={handleChange}>
-                <BottomNavigationAction
-                    onClick={() => history.push('/')}
-                    label="Home"
-                    value="home"
-                    icon={<HomeIcon />}
-                />
-                <BottomNavigationAction
-                    onClick={() => history.push('/profile-page')}
-                    label="Profile"
-                    value="profile"
-                    icon={<AccountIcon />}
-                />
-                <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-                <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-            </BottomNavigation>
+        <div>
+            <Hidden mdDown>
+                <Drawer className={classes.drewer} variant="permanent" anchor="left">
+                    <List>
+                        <ListItem button onClick={() => history.push('/')}>
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="home" />
+                        </ListItem>
+                        <ListItem button onClick={() => history.push('/profile-page')}>
+                            <ListItemIcon>
+                                <AccountIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="profile" />
+                        </ListItem>
+                    </List>
+                </Drawer>
+            </Hidden>
+            <Hidden lgUp>
+                <BottomNavigation className={classes.bottomNavigation} value={value} onChange={handleChange}>
+                    <BottomNavigationAction
+                        onClick={() => history.push('/')}
+                        label="Home"
+                        value="home"
+                        icon={<HomeIcon />}
+                    />
+                    <BottomNavigationAction
+                        onClick={() => history.push('/profile-page')}
+                        label="Profile"
+                        value="profile"
+                        icon={<AccountIcon />}
+                    />
+                    <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+                    <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+                </BottomNavigation>
+            </Hidden>
         </div>
     );
 }
