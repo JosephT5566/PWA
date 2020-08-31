5adca3a2acd176f8fbb4b9a1ea954274ef24c125
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-08-31 18:11:44 +0800

    Get youtube video infos 1. get youtube video infos from gapi after button clicked 2. generate iframe with infos 3. move the youtube display to main page, instead of data page 4. setting construct with Grid

--- src/DataPage/index.jsx
+++ src/DataPage/index.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
 
 export default function ProfilePage() {
-    const onGetYoutubes = () => {
-        window.gapi.load('client', () => {
-            window.gapi.client
-                .init({
-                    apiKey: 'AIzaSyCrCFtI5SBI9NclTDJlPmIP80yzL-brpzM',
-                })
-                .then(() =>
-                    window.gapi.client.request({
-                        path:
-                            'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_XRq7JriAORvDe1lI1RAsA&maxResults=10&order=date',
-                    })
-                )
-                .then((response) => console.log(response.result));
-        });
-    };
     return (
         <div>
             <h2 className="ui h2">Data</h2>
             <Divider />
-            <Button variant="contained" color="primary" onClick={onGetYoutubes}>
-                Get youtube
-            </Button>
         </div>
     );
 }
--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,13 +16,113 @@ const useStyles = makeStyles((theme) => ({
             backgroundColor: 'OLIVEDRAB',
         },
     },
+    gridItem: {
+        display: 'flex',
+        justifyContent: 'center',
+        justifyItems: 'center',
+    },
+    content: {
+        padding: '10px 0',
+    },
 }));
 
 export default function MainPage() {
+    const [youtubeItems, setYoutubeItems] = useState(null);
     const classes = useStyles();
+
+    const onGetYoutubes = () => {
+        window.gapi.load('client', async () => {
+            await window.gapi.client.init({
+                apiKey: 'AIzaSyCrCFtI5SBI9NclTDJlPmIP80yzL-brpzM',
+            });
+            const response = await window.gapi.client.request({
+                path:
+                    'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_XRq7JriAORvDe1lI1RAsA&maxResults=10&order=date',
+            });
+            const items = response.result.items;
+            console.log(items);
+
+            setYoutubeItems(Object.values(items));
+        });
+    };
+
+    const renderButton = () => {
+        return (
+            <Button variant="contained" color="primary" onClick={onGetYoutubes}>
+                Get youtube
+            </Button>
+        );
+    };
+
+    const renderYoutubes = (items) => {
+        if (!items) return;
+        console.log(items);
+        return (
+            <div>
+                {items.map((item) => {
+                    // console.log(`https://www.youtube.com/watch?v=${item.id.videoId}`);
+                    return (
+                        <iframe
+                            title={item.snippet.title}
+                            src={`https://www.youtube.com/embed/${item.id.videoId}`}
+                            frameBorder="0"
+                            allowFullScreen
+                        ></iframe>
+                    );
+                })}
+            </div>
+        );
+    };
+
+    const renderFroggy = () => {
+        const froggys = [
+            {
+                src: 'asanQH0piQE',
+                title: '【呱吉】密室蟑螂殺人事件！諾基驚人的真相是？',
+            },
+            {
+                src: 'ixcWPjzMSOU',
+                title: '【呱吉直播】呱吉電台EP55：演技以外你還愛我嗎？',
+            },
+            {
+                src: '59EVB4GejGk',
+                title: '【呱吉】呱張新聞EP10：雪璋的顏色是極限的顏色',
+            },
+            {
+                src: '-rkheAdZx-A',
+                title: '【呱吉】人生晚長 EP95  :  煮一鍋好音樂',
+            },
+            {
+                src: 'UP4QSKK16Ds',
+                title: '【呱吉直播】陪你睡覺EP1：我選擇死亡',
+            },
+            {
+                src: 'C5kEJdyer00',
+                title: '【呱吉直播】政治電台EP12：轟隆隆隆衝衝衝拉風引擎火箭發動',
+            },
+        ];
+        return froggys.map((froggy, index) => (
+            <Grid className={classes.gridItem} item xs={12} key={index}>
+                <iframe
+                    title={froggy.title}
+                    src={`https://www.youtube.com/embed/${froggy.src}`}
+                    frameBorder="0"
+                    allowFullScreen
+                ></iframe>
+            </Grid>
+        ));
+    };
+
     return (
-        <div className={classes.root}>
+        <div>
             <h2 className="ui h2">MainPage</h2>
+            <Divider />
+            {/* {renderButton()} */}
+            <div className={classes.content}>
+                <Grid container spacing={3}>
+                    {renderFroggy()}
+                </Grid>
+            </div>
         </div>
     );
 }
