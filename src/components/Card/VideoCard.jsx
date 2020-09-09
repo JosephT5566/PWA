5e89b8acc932e4092d7d44040ccc2953db6529f1
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-09 15:13:48 +0800

    Modify VideoCard

--- src/components/Card/VideoCard.jsx
+++ src/components/Card/VideoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     card: {
         width: '100%',
         paddingBottom: '56.25%',
@@ -12,27 +12,16 @@ const useStyles = makeStyles((theme) => ({
         position: 'relative',
     },
     image: {
-        width: '100%',
         position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 0, // 水平垂直置中內容
+        top: 0,
     },
 }));
 
-export default function ImageCard({ title, video }) {
+export default function ImageCard({ src }) {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            <iframe
-                className={classes.image}
-                title="【呱吉】呱張新聞EP10：雪璋的顏色是極限的顏色"
-                height="100%"
-                src={video}
-                frameBorder="0"
-                allowFullScreen
-            />
+            <iframe className={classes.image} width="100%" height="100%" src={src} frameBorder="0" allowFullScreen />
         </Card>
     );
 }
