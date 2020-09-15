0b9dd8376bb8e70184ac7e123df7ad3b785bb8b9
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-15 12:05:55 +0800

    Create Plot page 1. use Plot page to display line chart

---
+++ src/views/DataPage/Plot.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ArrowBackTitle from '../../components/Title/ArrowBackTitle';
+import Victory from '../../components/Victory';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+
+const useStyle = makeStyles(() => ({
+    content: {
+        padding: '10px 0',
+    },
+}));
+
+export default function Plot({ id }) {
+    const classes = useStyle();
+    return (
+        <div>
+            <ArrowBackTitle title={id} />
+            <Divider />
+            <div className="ui container">
+                <div className={classes.content}>
+                    <Victory />
+                </div>
+            </div>
+        </div>
+    );
+}
