9d730d6c9648a677710f9fc013a6987631bb3098
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-23 14:24:51 +0800

    Style Title with scss instead of material ui

--- src/components/Title/ArrowBackTitle.jsx
+++ src/components/Title/ArrowBackTitle.jsx
@@ -3,39 +3,19 @@ import { useNavigation } from 'react-navi';
 import Button from '@material-ui/core/Button';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    titleContainer: {
-        display: 'flex',
-        position: 'relative',
-        [theme.breakpoints.up('lg')]: {
-            flexDirection: 'left',
-        },
-        [theme.breakpoints.down('md')]: {
-            flexDirection: 'column',
-            alignItems: 'center',
-        },
-    },
-    button: {
-        [theme.breakpoints.down('md')]: {
-            position: 'absolute',
-            left: '0',
-        },
-    },
-}));
+import './styles.scss';
 
 export default function ArrowBackTitle(props) {
     const { title } = props;
-    const classes = useStyles();
     const navigation = useNavigation();
 
     return (
-        <div className={classes.titleContainer}>
-            <Button className={classes.button} onClick={() => navigation.goBack()}>
+        <div className="title">
+            <Button className="button" onClick={() => navigation.goBack()}>
                 <ArrowBack />
             </Button>
-            <Typography variant="h4">{title}</Typography>
+            <Typography className="label" variant="h4">{title}</Typography>
         </div>
     );
 }
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const useStyle = makeStyles((theme) => ({
-    title: {
-        display: 'flex',
-        flexDirection: 'column',
-        [theme.breakpoints.up('lg')]: {
-            alignItems: 'left',
-        },
-        [theme.breakpoints.down('md')]: {
-            alignItems: 'center',
-        },
-    },
-}));
+import './styles.scss';
 
 export default function Title({ title }) {
-    const classes = useStyle();
     return (
-        <div className={classes.title}>
-            <Typography variant="h4">{title}</Typography>
+        <div className="title">
+            <Typography className="label" variant="h4">{title}</Typography>
         </div>
     );
 }
