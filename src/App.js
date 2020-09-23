65ce1ca4faa3144b6a1fc1229e5fe7e2ce229aca
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-23 16:06:18 +0800

    Import Main component 1. Import Main component to App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import React, { Suspense } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Router, View } from 'react-navi';
-import Grid from '@material-ui/core/Grid';
-import HelmetProvider from 'react-navi-helmet-async';
+import React from 'react';
 import './i18n';
 
-import routes from './routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Navigation from './components/Navigation';
+import Main from './components/Main';
 
 import './App.scss';
-
-const basename = process.env.PUBLIC_URL;
 
 function App() {
     return (
         <div className="app">
             <Header />
-            <div className="main">
-                <HelmetProvider>
-                    <Router routes={routes} basename={basename}>
-                        <Grid container>
-                            <Grid className="nav" item lg={2} xs={12}>
-                                <Navigation basename={basename} />
-                            </Grid>
-                            <Grid className="content" item lg={10} xs={12}>
-                                <Suspense fallback={null}>
-                                    <View />
-                                </Suspense>
-                            </Grid>
-                        </Grid>
-                    </Router>
-                </HelmetProvider>
-            </div>
+            <Main />
             <Footer />
         </div>
     );
