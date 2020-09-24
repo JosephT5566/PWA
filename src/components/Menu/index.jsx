a76b51d16232bb9a4450ed24a0815525a47d3888
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-24 16:08:23 +0800

    Modify Menu 1. remove onClick method, user can define the method in items.onClick 2. change <a/> to <button/> 3. style the button

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import { useNavigation } from 'react-navi';
 
 import './styles.scss';
 
 export default function Menu({ items = [] }) {
-    const navigation = useNavigation();
-    const currentURL = navigation.getCurrentValue().url.pathname;
-
     return (
         <nav id="menu">
             <ul>
-                {items.map(({ label = '', tab }, index) => {
+                {items.map(({ label = '', onClick = null }, index) => {
                     return (
                         <li key={index}>
-                            <a
-                                onClick={() => {
-                                    if (tab) {
-                                        navigation.navigate(`${currentURL}/${tab}`);
-                                    }
-                                }}
-                            >
-                                {label}
-                            </a>
+                            <button onClick={onClick}>{label}</button>
                         </li>
                     );
                 })}
