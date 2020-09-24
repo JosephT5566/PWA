0439242bc83af70ccfce039855a07db84b4dbcb0
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-09-24 18:38:37 +0800

    Change title component 1. use h1 instead of Typography

--- src/components/Title/ArrowBackTitle.jsx
+++ src/components/Title/ArrowBackTitle.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useNavigation } from 'react-navi';
 import Button from '@material-ui/core/Button';
 import ArrowBack from '@material-ui/icons/ArrowBack';
-import Typography from '@material-ui/core/Typography';
 
 import './styles.scss';
 
@@ -15,7 +14,7 @@ export default function ArrowBackTitle(props) {
             <Button className="button" onClick={() => navigation.goBack()}>
                 <ArrowBack />
             </Button>
-            <Typography className="label" variant="h4">{title}</Typography>
+            <h1 className="label">{title}</h1>
         </div>
     );
 }
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 
 import './styles.scss';
 
 export default function Title({ title }) {
     return (
         <div className="title">
-            <Typography className="label" variant="h4">{title}</Typography>
+            <h1 className="label">{title}</h1>
         </div>
     );
 }
