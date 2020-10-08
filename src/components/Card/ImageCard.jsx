7d30d95aa0b985051841ebd1c20176c146194d5f
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-10-08 15:56:02 +0800

    Feat: Class name of ImageCard 1. add className props to ImageCard

--- src/components/Card/ImageCard.jsx
+++ src/components/Card/ImageCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './imageCard.scss';
 
-export default function ImageCard({ label, image = null, onClick }) {
+export default function ImageCard({ className, label, image = null, onClick }) {
     const renderImage = () => {
         if (image) {
             return <div style={{ backgroundImage: `url(${image})` }} />;
         }
     };
     return (
-        <button id="imageCard" onClick={onClick}>
+        <button id="imageCard" className={className} onClick={onClick}>
             <h2>{label}</h2>
             {renderImage()}
         </button>
