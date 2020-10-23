e75563ee67a90d1333552386fd4e7b30e4bbb288
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-10-23 18:28:28 +0800

    Feat: create types.js 1. create types for constant values

---
+++ src/assets/types.js
@@ -0,0 +1,37 @@
+export const BACKEND_URL = 'http://localhost:9527';
+
+export const DEFAULT_IMAGE = '';
+
+export const USER_TYPE = {
+    id: 'id',
+    username: 'username',
+    password: 'password',
+    email: 'email',
+    firstname: 'firstname',
+    lastname: 'lastname',
+    address: 'address',
+    postal: 'postal',
+    county: 'county',
+    city: 'city',
+    country: 'country',
+    phone: 'phone',
+    birth: 'birth',
+    govIDFrontImg: 'govIDFrontImg',
+    govIDBackImg: 'govIDBackImg',
+};
+
+export const CARD_TYPE = {
+    id: 'id',
+    userID: 'userID',
+    cardName: 'cardName',
+    account: 'account',
+    bank: 'bank',
+    branch: 'branch',
+    onlineAccount: 'onlineAccount',
+    onlinePsd: 'onlinePsd',
+    paymentPsd: 'paymentPsd',
+    pin: 'pin',
+    securityCode: 'securityCode',
+    cardBackImg: 'cardBackImg',
+    cardFrontImg: 'cardFrontImg',
+};
