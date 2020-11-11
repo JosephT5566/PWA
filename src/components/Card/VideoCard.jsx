cb590c12fd3163db0b4fb22b073eb16ceee2fd6f
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-11-11 18:25:52 +0800

    Feat: Modify VideoCard 1. change the structure 2. change the style by scss

--- src/components/Card/VideoCard.jsx
+++ src/components/Card/VideoCard.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 
-const useStyles = makeStyles(() => ({
-    card: {
-        width: '100%',
-        paddingBottom: '56.25%',
-        boxShadow: '0 8px 24px 0 rgba(0,0,0,0.12)',
-        borderRadius: '1.5rem',
-        overflow: 'hidden',
-        position: 'relative',
-    },
-    image: {
-        position: 'absolute',
-        top: 0,
-    },
-}));
+import './videoCard.scss';
 
-export default function ImageCard({ src }) {
-    const classes = useStyles();
+export default function VideoCard({ src }) {
     return (
-        <Card className={classes.card}>
-            <iframe className={classes.image} width="100%" height="100%" title=" " src={src} frameBorder="0" allowFullScreen />
-        </Card>
+        <div id="videoCard">
+            <div className="video">
+                <div className="iframe">
+                    <iframe width="100%" height="100%" src={src} frameBorder="0" allowFullScreen />
+                </div>
+            </div>
+        </div>
     );
 }
